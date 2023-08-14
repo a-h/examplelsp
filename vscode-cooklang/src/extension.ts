fdc0739438777586201a54b6c5abedf3ea040518
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-08-14 19:38:27 +0100

    feat: add vscode extension

--- vscode-cooklang/src/extension.ts
+++ vscode-cooklang/src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
+  vscode.window.showInformationMessage(`Starting LSP`);
+  let serverOptions: ServerOptions = {
+	command: "examplelsp",
+	transport: TransportKind.stdio,
+  };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-cooklang" is now active!');
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "cook" }],
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-cooklang.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-cooklang!');
-	});
+  client = new LanguageClient(
+    "cook",
+    "cook",
+    serverOptions,
+    clientOptions
+  );
 
-	context.subscriptions.push(disposable);
+  client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
